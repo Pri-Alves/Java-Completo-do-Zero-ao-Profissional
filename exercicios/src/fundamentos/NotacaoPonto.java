4e8418d48c0bb97734a6efceb57a6dc8f56a73e8
Pri-Alves <priscillarafaela19@gmail.com>
2021-10-18 10:52:29 -0300

    Notacao Ponto

---
+++ exercicios/src/fundamentos/NotacaoPonto.java
@@ -0,0 +1,24 @@
+package fundamentos;
+
+import java.util.Locale;
+
+public class NotacaoPonto {
+    public static void main(String[] args) {
+        String s = "Bom dia X";
+        s = s.replace("X", "senhora");
+        s = s.toUpperCase();
+        s = s.concat("!!!");
+
+        System.out.println(s);
+
+        String x = "Leo".toUpperCase();
+        System.out.println(x);
+
+        String y = "Bom dia X".replace("X", "Gui").toUpperCase().concat("!!!");
+        System.out.println(y);
+
+        // Tipos primitivos nao te o operdor "."
+        int a = 3;
+        System.out.println(a);
+    }
+}
